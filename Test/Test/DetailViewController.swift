a77a3c51ee88b99f2651aa1ad38a82376f83faee
Brandon Titus <bjtitus@gmail.com>
2015-11-03 22:46:42 -0700

    Remove log

--- Test/Test/DetailViewController.swift
+++ Test/Test/DetailViewController.swift
@@ -24,7 +24,6 @@ class DetailViewController: UIViewController {
         if let detail = self.detailItem {
             if let label = self.detailDescriptionLabel {
                 label.text = detail.description
-                print("Something")
             }
         }
     }
